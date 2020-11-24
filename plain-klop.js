8fd994e99dff1e1b2aca21663e78e72f50de1014
pedroxian <44950875+pedroxian@users.noreply.github.com>
2020-11-24 01:35:34 +0100

    Add files via upload

---
+++ plain-klop.js
@@ -0,0 +1,15 @@
+{ScriptProcessorNode}ActiveXObject Node.js app isFinite.level pharse.else if.true
+    function active port.ping program-run if.ww {HTMLOptGroupElement}
+
+        {
+            MediaStreamAudioDestinationNode.infinite warfare.knop
+        }
+
+            MediaStreamTrackAudioSourceNode toString.gov  else function
+
+                to .gogo if port.sequence after.bruteforce if.gogo
+
+                    {
+                        MediaDeviceInfo.debian look.configuration   MutationRecord:MODE
+                            debian.resource go.js after.loop {eclect}{execute
+                    }
